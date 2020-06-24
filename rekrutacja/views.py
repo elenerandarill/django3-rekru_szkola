daf46509d10977bb47ad363bdd848daf40430a4f
elener <paulina.wojno@gmail.com>
2020-06-24 18:25:04 +0200

    form but no model yet

---
+++ rekrutacja/views.py
@@ -0,0 +1,62 @@
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm, FormUczen
+
+
+def home(request):
+    title = 'Strona Glowna'
+    return render(request, 'rekrutacja/home.html', {'title': title})
+
+
+def rejestracja(request):
+    title = 'Rejestracja'
+    if request.method == 'GET':
+        form = UserRegisterForm()
+        return render(request, 'rekrutacja/rejestracja.html', {'title': title, 'form': form})
+    else:
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Konto zostało utworzone. Możesz się zalogować.')
+            return redirect('home')
+
+
+def logowanie(request):
+    title = 'Logowanie'
+    form = AuthenticationForm()
+    if request.method == 'GET':
+        return render(request, 'rekrutacja/logowanie.html', {'title': title, 'form': form})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Login i hasło nie pasują.')
+            return render(request, 'rekrutacja/logowanie.html', {'title': title, 'form': form})
+
+
+@login_required
+def wyloguj(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+
+@login_required
+def aplikuj(request):
+    title = 'Aplikuj'
+    return render(request, 'rekrutacja/aplikuj.html', {'title': title})
+
+
+@login_required
+def formuczen(request):
+    title = 'Uczniowie'
+    form = FormUczen()
+    if request.method == 'GET':
+        return render(request, 'rekrutacja/formuczen.html', {'title': title})
+    else:
+        pass
